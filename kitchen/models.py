e9f268f95ffd155e1f6f57074cedee45209af360
P.V.S.1905 <viktor66462@gmail.com>
2025-02-07 20:23:34 +0200

    Add initial models for Cook, DishType, Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return  f"{self.name}: {self.description} - {self.price}"
