ef5c3e6c4eab43e87d0904c215ba453a368eef0f
P.V.S.1905 <viktor66462@gmail.com>
2025-02-08 19:36:08 +0200

    Create home page for Kitchen Service

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from kitchen.models import Cook, DishType, Dish
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dish_type = DishType.objects.count()
+    num_dish = Dish.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_type": num_dish_type,
+        "num_dish": num_dish,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
+
