60ac9dc3c5077dc3278ac306bff9b2849003d84b
P.V.S.1905 <viktor66462@gmail.com>
2025-02-08 17:40:45 +0200

    Add admin page and update models

--- kitchen/models.py
+++ kitchen/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True, blank=True)
 
     class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
         ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
 
 
 class DishType(models.Model):
